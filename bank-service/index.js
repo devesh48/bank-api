afe5398596e296cfc5bb3e418ac565e9d8d93643
devesh48 <upadhyay.dev48@gmail.com>
2024-05-20 22:32:57 -0400

    database connection code

--- bank-service/index.js
+++ bank-service/index.js
@@ -1,7 +1,14 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import { configDotenv } from 'dotenv'
+configDotenv();
+mongoose.connect(process.env.Mongo).then(() => {
+    console.log("Connected to the database!!");
+}).catch((err) => {
+    console.log(err);
+});
 const app = express();
 
 app.listen(3002, () => {
-    console.log('Server is listening on port 3002')
-})
+    console.log('Server is listening on port 3002!')
+});
