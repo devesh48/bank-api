fed2e57ffde4831ff30cd240908004148fa1fd5e
devesh48 <upadhyay.dev48@gmail.com>
2024-05-21 16:16:56 -0400

    updating google signon

--- bank-service/controllers/auth.controller.js
+++ bank-service/controllers/auth.controller.js
@@ -23,18 +23,41 @@ export const signIn = async (req, res, next) => {
     try {
         console.log(req.body);
         const { email, password } = req.body;
-        const validUser = await User.findOne({ email: email});
-        if (!validUser){
+        const validUser = await User.findOne({ email: email });
+        if (!validUser) {
             return next(errorHandler(401, 'Invalid Email Credentials'));
         }
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword){
+        if (!validPassword) {
             return next(errorHandler(401, "incorrect password"));
         }
-        const expiryDate = new Date( Date.now() + 36000000);
-        const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
-        const {password: hashedPwd, ...rest} = validUser._doc;
-        res.cookie('access_token', token, {httpOnly: true, expires: expiryDate}).status(200).json(rest);
+        const expiryDate = new Date(Date.now() + 36000000);
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hashedPwd, ...rest } = validUser._doc;
+        res.cookie('access_token', token, { httpOnly: true, expires: expiryDate }).status(200).json(rest);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const googleAuth = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (user) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const { password: hashedPwd, ...rest } = user._doc;
+            const expiryDate = new Date(Date.now() + 36000000);
+            res.cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+        } else {
+            const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+            const hashedPwd = bcryptjs.hashSync(generatedPassword, 10);
+            const newUser = new User({ username: req.body.name.split(" ").join("").toLowerCase() + Math.floor(Math.random() * 100000).toString(), email: req.body.email, password: hashedPwd, profilePicture: req.body.photo });
+            await newUser.save();
+            const expiryDate = new Date(Date.now() + 36000000);
+            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const { password: hashedPwd2, ...rest } = newUser._doc;
+            res.cookie('access_token', token, { httpOnly: true, expires: expiryDate }).status(200).json(rest);
+        }
     } catch (err) {
         next(err);
     }
--- bank-service/models/user.models.js
+++ bank-service/models/user.models.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique:true
+    },
+    profilePicture:{
+        type: String,
+        default:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F5%2FProfile.png&f=1&nofb=1&ipt=04ef65619b79ae1aa77f1add50da6a6117bcd5425b2ba7b4a6f076cfc8c5c773&ipo=images'
     }
 }, {timestamps: true});
 
