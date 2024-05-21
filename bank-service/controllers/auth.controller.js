07725c51762f1fbca20a76ef5b636c37143639c2
devesh48 <upadhyay.dev48@gmail.com>
2024-05-21 02:22:59 -0400

    create signin route

--- bank-service/controllers/auth.controller.js
+++ bank-service/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.models.js'
 import bcryptjs from 'bcryptjs'
-// import { errorHandler } from '../utils/error.js';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signUp = async (req, res, next) => {
     try {
@@ -14,5 +15,27 @@ export const signUp = async (req, res, next) => {
         next(err);
         // next(errorHandler(300, "something went wrong"));
         // res.status(500).send(err.message);
+    }
+};
+
+
+export const signIn = async (req, res, next) => {
+    try {
+        console.log(req.body);
+        const { email, password } = req.body;
+        const validUser = await User.findOne({ email: email});
+        if (!validUser){
+            return next(errorHandler(401, 'Invalid Email Credentials'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword){
+            return next(errorHandler(401, "incorrect password"));
+        }
+        const expiryDate = new Date( Date.now() + 36000000);
+        const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
+        const {password: hashedPwd, ...rest} = validUser._doc;
+        res.cookie('access_token', token, {httpOnly: true, expires: expiryDate}).status(200).json(rest);
+    } catch (err) {
+        next(err);
     }
 };
